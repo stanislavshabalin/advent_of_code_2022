b3732448fca60dec68c5fd75205e4ac0f52c779f
Stanislav Shabalin <services@voxpuibr.com>
2022-12-03 02:18:19 +0100

    Add solutions for Day 1

---
+++ code/01_1.py
@@ -0,0 +1,23 @@
+import os
+
+
+def main(input_file):
+    max_calories = 0
+    current = 0
+    for line in input_file:
+        calories = None
+        line = line.strip()
+        if line:
+            calories = int(line, 10)
+            current += calories
+        else:
+            max_calories = max(max_calories, current)
+            current = 0
+
+    print(max_calories)
+
+
+if __name__ == "__main__":
+    file_path = os.path.join(os.path.dirname(__file__), "01_1.txt")
+    with open(file_path, "r") as input_file:
+        main(input_file)
---
+++ code/01_2.py
@@ -0,0 +1,26 @@
+import os
+
+
+def main(input_file):
+    POOL_SIZE = 3
+    pool = []
+
+    current = 0
+    for line in input_file:
+        calories = None
+        line = line.strip()
+        if line:
+            calories = int(line, 10)
+            current += calories
+        else:
+            pool.append(current)
+            pool = list(sorted(pool, reverse=True)[:POOL_SIZE])
+            current = 0
+
+    print(sum(pool))
+
+
+if __name__ == "__main__":
+    file_path = os.path.join(os.path.dirname(__file__), "01_2.txt")
+    with open(file_path, "r") as input_file:
+        main(input_file)
